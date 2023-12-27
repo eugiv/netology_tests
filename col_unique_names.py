44e6067c61009a9a81c9c691a13cbad3baee9e26
eugiv <eugene.ivanov@ymail.com>
2023-12-28 01:58:32 +0300

    mid progress

--- col_unique_names.py
+++ col_unique_names.py
@@ -1,8 +1,4 @@
 
-
-courses = ["Python-разработчик с нуля", "Java-разработчик с нуля", "Fullstack-разработчик на Python",
-           "Frontend-разработчик с нуля"]
-
 mentors = [
     ["Евгений Шмаргунов", "Олег Булыгин", "Дмитрий Демидов", "Кирилл Табельский", "Александр Ульянцев",
      "Александр Бардин", "Александр Иванов", "Антон Солонилин", "Максим Филипенко", "Елена Никитина", "Азамат Искаков",
@@ -19,19 +15,40 @@ mentors = [
 ]
 
 
-all_list = []
-for m in mentors:
-    all_list.extend(m)
+def flat_init_list(init_list: list):
+    full_list = []
+    for m in init_list:
+        full_list.extend(m)
 
-all_names_list = []
-for mentor in all_list:
-    mentor = list([mentor])
-    for name in mentor:
-        name = name.split()[0]
-    all_names_list.append(name)
+    return full_list
 
-unique_names = list(set(all_names_list))
 
-all_names_sorted = sorted(unique_names)
-separator = ", "
-print(f'Уникальные имена преподавателей: {separator.join(all_names_sorted)}')
+all_list = flat_init_list(mentors)
+
+
+def extract_names(full_list: list):
+    all_names_list = []
+    for mentor in full_list:
+        mentor = list([mentor])
+        for name in mentor:
+            name = name.split()[0]
+        all_names_list.append(name)
+
+    return all_names_list
+
+
+all_names_list = extract_names(all_list)
+
+
+def unique_names(names_list: list):
+    uni_names = list(set(names_list))
+
+    all_names_sorted = sorted(uni_names)
+    separator = ", "
+    # print(f'Уникальные имена преподавателей: {separator.join(all_names_sorted)}')
+    print(all_names_sorted)
+
+    return all_names_sorted
+
+
+unique_names(all_names_list)
---
+++ tests/test_col_unique_names.py
@@ -0,0 +1,44 @@
+import pytest
+
+from col_unique_names import flat_init_list, extract_names, unique_names
+
+test_list =[
+    ["Евгений Шмаргунов", "Олег Булыгин"],
+    ["Филипп Воронов", "Анна Юшина"],
+    ["Евгений Шмаргунов", "Александр Бардин"],
+]
+
+
+def test_flat_init_list():
+    result = flat_init_list(test_list)
+    expected = [
+    "Евгений Шмаргунов", "Олег Булыгин",
+    "Филипп Воронов", "Анна Юшина",
+    "Евгений Шмаргунов", "Александр Бардин",
+]
+    assert result == expected
+
+
+test_flat_result = flat_init_list(test_list)
+
+
+def test_extract_names():
+    result = extract_names(test_flat_result)
+    expected = [
+    "Евгений", "Олег",
+    "Филипп", "Анна",
+    "Евгений", "Александр",
+]
+    assert result == expected
+
+
+test_extract_res = extract_names(test_flat_result)
+
+def test_unique_names():
+    result = unique_names(test_extract_res)
+    expected = [
+    "Александр", "Анна",
+    "Евгений", "Олег",
+    "Филипп",
+]
+    assert result == expected
